aa32fd8e0f1452425ee8a397ecce70a3836e82e3
yohanna4 <yohannayim4@gmail.com>
2023-03-11 19:12:15 +0300

    .

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,9 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:learncoding/theme/config.dart' as config;
 
 // IMPOTANT: We are not using this themeData anywhere in the app.
 // the theme to the cupertino app is provided in the main.dart file.
 // and the font family 'Red Hat Display' is no longer suppoted in this codebase.
+class ThemeProvider extends ChangeNotifier {
+  ThemeMode themeMode = ThemeMode.system;
+
+  bool get isDarkMode {
+    if (themeMode == ThemeMode.system) {
+      final brightness = SchedulerBinding.instance?.window.platformBrightness;
+      return brightness == Brightness.dark;
+    } else {
+      return themeMode == ThemeMode.dark;
+    }
+  }
+
+  void toggleTheme(bool isOn) {
+    themeMode = isOn ? ThemeMode.dark : ThemeMode.light;
+    notifyListeners();
+  }
+}
+
+class Themes {
+  static final lightTheme = ThemeData(
+    appBarTheme:
+        AppBarTheme(backgroundColor: Color.fromARGB(255, 124, 61, 200)),
+    scaffoldBackgroundColor: Colors.white,
+    primaryColor: Colors.white,
+    colorScheme: const ColorScheme.light(),
+    // textTheme: TextTheme(),
+
+    iconTheme: const IconThemeData(color: Colors.white),
+    buttonTheme:
+        ButtonThemeData(buttonColor: Color.fromARGB(255, 182, 114, 246)),
+  );
+  static final darkTheme = ThemeData(
+    textTheme: const TextTheme(),
+    buttonTheme: ButtonThemeData(buttonColor: Color.fromARGB(255, 75, 51, 79)),
+    scaffoldBackgroundColor: Colors.grey.shade900,
+    primaryColor: Colors.black,
+    colorScheme: const ColorScheme.dark(),
+    iconTheme: const IconThemeData(color: Colors.white),
+  );
+}
 
 var kLightTheme = ThemeData(
   canvasColor: Colors.transparent,
